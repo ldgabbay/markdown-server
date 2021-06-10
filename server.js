429c7ff3907135dd58024ff124df6bd62f247f52
Lynn Gabbay <gabbay@gmail.com>
2021-06-10 16:50:32 +0300

    cosmetic

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ function wrap(pathElements, body) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/fonts.css">
-    <link rel="stylesheet" href="/markdown.css">
+    <link rel="stylesheet" href="/fonts.css" />
+    <link rel="stylesheet" href="/markdown.css" />
     <link rel="stylesheet" href="/prism.css" />
     ${localCss}
 </head>
