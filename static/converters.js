d6d4f07fec17552ea413b427fb6f588b75ac026e
Lynn Gabbay <gabbay@gmail.com>
2021-12-17 11:48:39 -0500

    display tweaks

--- static/converters.js
+++ static/converters.js
@@ -6,13 +6,15 @@ function fixedEncodeURIComponent(str) {
 
 document.addEventListener('DOMContentLoaded', function() {
   for (const converter of ['texmath', 'puml']) {
-    let nodes = document.getElementsByClassName(`language-${converter}`);
-    // prism.js adds the classname to the <pre> parent element too, which breaks this.  so load this script first.
-    for (let node of nodes) {
+    while (true) {
+      let nodes = document.getElementsByClassName(`language-${converter}`);
+      // prism.js adds the classname to the <pre> parent element too, which breaks this.  so load this script first.
+      if (nodes.length === 0) break;
+      let node = nodes[0];
       let img = document.createElement('img');
       img.src = `/_/${converter}/${fixedEncodeURIComponent(node.innerText.replace(/^\s+|\s+$/g, ''))}`;
       img.classList.add(converter);
-      let div = document.createElement('div');
+      let div = document.createElement('p');
       div.appendChild(img);
       let parent = node.parentElement;
       parent.parentElement.replaceChild(div, parent);
