69ef2f49ef2f33c412e34d9d473bac46d84159ae
Lynn Gabbay <gabbay@gmail.com>
2021-12-17 11:46:43 -0500

    tweaked texmath generation

--- etc/tex2svg.sh
+++ etc/tex2svg.sh
@@ -3,6 +3,6 @@
 X=$(mktemp -d)
 cat > $X/file.tex
 latex -output-directory $X $X/file.tex &>/dev/null
-dvisvgm --no-fonts -o $X/%f $X/file.dvi &>/dev/null
+dvisvgm --no-fonts -Z 1.5 -o $X/%f $X/file.dvi &>/dev/null
 cat $X/file.svg
 rm -rf $X
